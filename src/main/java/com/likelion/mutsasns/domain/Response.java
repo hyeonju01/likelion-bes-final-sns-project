67c79ac38f10af7339d6ada23c527fc5c6746864
hyeonju01 <tangled6648@gmail.com>
2022-12-22 00:57:33 +0900

    feat : Response<T>
    - refactor: change error methods

---
+++ src/main/java/com/likelion/mutsasns/domain/Response.java
@@ -0,0 +1,27 @@
+package com.likelion.mutsasns.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/*
+22. 12. 21 변경
+ */
+@AllArgsConstructor
+@Getter
+public class Response<T> {
+
+    private String resultCode;
+    private T result;
+
+    public static <T> Response<T> error(String resultCode,  T result) {
+        return new Response(resultCode, result);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response<>("SUCCESS",  result);
+    }
+
+    public static Response<Void> success() {
+        return new Response("SUCCESS",  null);
+    }
+}
