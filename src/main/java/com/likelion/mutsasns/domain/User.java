b7c46b194a3b85cce0318a043e0c4d5e55803a85
hyeonju01 <tangled6648@gmail.com>
2022-12-22 01:04:23 +0900

    feat : Create a User Entity

---
+++ src/main/java/com/likelion/mutsasns/domain/User.java
@@ -0,0 +1,26 @@
+package com.likelion.mutsasns.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //id 생성 전략은 DB에 위임
+    private Long id;
+
+    @Column(unique = true) //userName은 고유해야한다.
+    private String userName;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
