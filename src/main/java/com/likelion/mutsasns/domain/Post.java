aa2402c0df11041403c7ad36365e2887f5c77ad1
hyeonju01 <tangled6648@gmail.com>
2022-12-26 10:31:26 +0900

    Chore: Add annotations

--- src/main/java/com/likelion/mutsasns/domain/Post.java
+++ src/main/java/com/likelion/mutsasns/domain/Post.java
@@ -1,4 +1,15 @@
 package com.likelion.mutsasns.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@ToString
 public class Post {
+
 }
