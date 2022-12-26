32d610d745ed528c2359d6d05bd73c3fe143bde2
hyeonju01 <tangled6648@gmail.com>
2022-12-26 10:29:59 +0900

    feat: add sqlExceptionHandler

--- src/main/java/com/likelion/mutsasns/exception/ExceptionManager.java
+++ src/main/java/com/likelion/mutsasns/exception/ExceptionManager.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+
 /*
 [  ] 22.12.22 ErrorResponse 이렇게 하는 것 맞나....잘 모르겠다
 [ V ] 22.12.21 프로젝트 로그에 강사님이 올려주신 코드와 비교
@@ -16,6 +19,13 @@ public class ExceptionManager {
     public ResponseEntity<?> SnsExceptionHandler(SnsException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode(), e.getMessage());
         return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(Response.error("ERROR", errorResponse));
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> sqlExceptionHandler(SQLException e) {
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.DATABASE_ERROR, e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Response.error("ERROR", errorResponse));
     }
 
