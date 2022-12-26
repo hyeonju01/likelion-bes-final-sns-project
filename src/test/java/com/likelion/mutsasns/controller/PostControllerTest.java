84b337d31716a6fd28f4a18bae8dba1c6c03ac2a
hyeonju01 <tangled6648@gmail.com>
2022-12-26 10:32:11 +0900

    Test: Add a PostControllerTest.java

--- src/test/java/com/likelion/mutsasns/controller/PostControllerTest.java
+++ src/test/java/com/likelion/mutsasns/controller/PostControllerTest.java
@@ -1,7 +1,22 @@
 package com.likelion.mutsasns.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+annotation 적절한 것으로 달았는지 확인
+ */
 class PostControllerTest {
+    @Test
+    @DisplayName("포스트 작성 성공")
+    void post_success() {
+    }
 
+    @Test
+    @DisplayName("포스트 작성 실패 - 로그인 상태가 아닐 경우")
+    void post_fail() {
+    }
+    
 }
