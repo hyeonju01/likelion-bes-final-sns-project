a1229a883512b8e44807cb6e14a07a5c74e909a5
hyeonju01 <tangled6648@gmail.com>
2022-12-26 16:57:22 +0900

    Feat: Add PostAddRequest.java

---
+++ src/main/java/com/likelion/mutsasns/domain/dto/PostAddRequest.java
@@ -0,0 +1,22 @@
+package com.likelion.mutsasns.domain.dto;
+
+import com.likelion.mutsasns.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class PostAddRequest {
+    private String title; //포스트 제목
+    private String body; //포스트 내용
+
+    public Post toEntity() {
+        Post post = Post.builder()
+                .title(this.title)
+                .body(this.body)
+                .build();
+        return post;
+    }
+}
