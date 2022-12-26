ec249d9e334e56e84917455d46fd1617d31f4bb3
hyeonju01 <tangled6648@gmail.com>
2022-12-26 16:58:11 +0900

    Feat: Add PostService.java

--- src/main/java/com/likelion/mutsasns/service/PostService.java
+++ src/main/java/com/likelion/mutsasns/service/PostService.java
@@ -1,4 +1,25 @@
 package com.likelion.mutsasns.service;
 
+import com.likelion.mutsasns.domain.Post;
+import com.likelion.mutsasns.domain.dto.PostAddRequest;
+import com.likelion.mutsasns.domain.dto.PostAddResponse;
+import com.likelion.mutsasns.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public PostAddResponse add(PostAddRequest dto) {
+        Post post = dto.toEntity();
+        Post registerdPost = postRepository.save(post);
+        return new PostAddResponse(registerdPost.getId(),
+                registerdPost.getTitle(), registerdPost.getBody());
+    }
+
 }
