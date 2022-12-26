6af79e537eaba2ac2786ef6230e748a4d777b410
hyeonju01 <tangled6648@gmail.com>
2022-12-26 16:58:38 +0900

    Test: PostControllerTest.java

--- src/test/java/com/likelion/mutsasns/controller/PostControllerTest.java
+++ src/test/java/com/likelion/mutsasns/controller/PostControllerTest.java
@@ -1,22 +1,89 @@
 package com.likelion.mutsasns.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likelion.mutsasns.domain.dto.PostAddRequest;
+import com.likelion.mutsasns.domain.dto.PostAddResponse;
+import com.likelion.mutsasns.exception.ErrorCode;
+import com.likelion.mutsasns.exception.SnsException;
+import com.likelion.mutsasns.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /*
-annotation 적절한 것으로 달았는지 확인
+- [ V ] 성공, 실패 테스트 성공
  */
+
+@WebMvcTest(PostController.class)
 class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    PostService postService;
+
     @Test
     @DisplayName("포스트 작성 성공")
-    void post_success() {
+    @WithMockUser(username = "hyeonju", password = "1234")
+    void post_success() throws Exception {
+
+        //Request 객체 생성
+        PostAddRequest dto = new PostAddRequest("제목", "내용");
+
+        //mock test 코드 작성
+        when(postService.add(any())).thenReturn(new PostAddResponse(1L, dto.getTitle(), dto.getBody()));
+
+        // 검증
+        mockMvc.perform(post("/api/v1/posts")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(dto))
+                        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.body").exists())
+                .andDo(print());
+
+        verify(postService).add(dto);
     }
 
     @Test
     @DisplayName("포스트 작성 실패 - 로그인 상태가 아닐 경우")
-    void post_fail() {
+    @WithAnonymousUser
+    void post_fail() throws Exception {
+        PostAddRequest dto = new PostAddRequest("제목", "내용");
+
+        //given(postService.add(any())).willReturn(new PostAddResponse(1l, dto.getTitle(), dto.getBody()));
+        when(postService.add(any())).thenThrow(new SnsException(ErrorCode.INVALID_PERMISSION, ""));
+
+        mockMvc.perform(post("/api/v1/posts")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(dto))
+                        )
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
     }
-    
+
 }
