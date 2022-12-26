6c7023ef2a092d07ce0029f81e03ce86d78cb798
hyeonju01 <tangled6648@gmail.com>
2022-12-26 16:58:20 +0900

    Feat: Add PostRepository.java

--- src/main/java/com/likelion/mutsasns/repository/PostRepository.java
+++ src/main/java/com/likelion/mutsasns/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.likelion.mutsasns.repository;
 
-public interface PostRepository {
+import com.likelion.mutsasns.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
