5abc80b2cad474ecc87cc087cf4b5ba48e92ff38
hyeonju01 <tangled6648@gmail.com>
2022-12-26 16:57:09 +0900

    Feat: Add Post Entity

--- src/main/java/com/likelion/mutsasns/domain/Post.java
+++ src/main/java/com/likelion/mutsasns/domain/Post.java
@@ -1,15 +1,31 @@
 package com.likelion.mutsasns.domain;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import com.likelion.mutsasns.domain.dto.PostDto;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
-//@Entity
-//@NoArgsConstructor
-//@Getter
-//@ToString
-//public class Post {
-//
-//}
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Post extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String body;
+
+    // [ ] User Entity와 mapping 필요
+    //private Long user_id;
+
+    // [V] of 메서드
+    public static PostDto of(Post post) {
+        return new PostDto(post.getId(), post.getTitle(), post.getBody());
+    }
+}
