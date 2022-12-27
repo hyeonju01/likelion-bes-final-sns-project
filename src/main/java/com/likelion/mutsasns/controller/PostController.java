ba82fd4bd9f1af9555dfe8f5bc8592d17244b290
hyeonju01 <tangled6648@gmail.com>
2022-12-27 15:36:44 +0900

    Feat: PostController.java
    - 포스트 등록 기능
    - 포스트 1건 조회 기능
    - 포스트 리스트 조회 기능
    - 포스트 수정 기능
    - 포스트 삭제 기능

--- src/main/java/com/likelion/mutsasns/controller/PostController.java
+++ src/main/java/com/likelion/mutsasns/controller/PostController.java
@@ -1,10 +1,8 @@
 package com.likelion.mutsasns.controller;
 
 
-import com.likelion.mutsasns.domain.Post;
 import com.likelion.mutsasns.domain.Response;
 import com.likelion.mutsasns.domain.dto.*;
-import com.likelion.mutsasns.repository.PostRepository;
 import com.likelion.mutsasns.service.PostService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,53 +21,55 @@ public class PostController {
 
     private final PostService postService;
 
-    private final PostRepository postRepository;
-
-//    // di -> @AllArgsConstructor 어노테이션으로 생략
-//    public PostController(PostService postService, PostRepository postRepository) {
-//        this.postService = postService;
-//        this.postRepository = postRepository;
-//    }
-
+    // 📌
     // [ V ] 포스트 등록 기능 구현
-    // [   ] return Response 형식 수정필요
+    // [ v ] Swagger Test Passed
+    // [ v ] return 형식 맞추기
     @PostMapping("")
     public ResponseEntity<Response<PostAddResponse>> registerPost(@RequestBody PostAddRequest dto) {
-        PostAddResponse response = postService.add(dto);
-        return ResponseEntity.ok().body(Response.success(response));
+        PostAddResponse postAddResponse = postService.add(dto);
+        return ResponseEntity.ok().body(Response.success(postAddResponse));
     }
 
-    // [  ] 포스트 조회 기능 (1건) - 구현중
-    // 테스트 필요
+    // 📌
+    // [ v ] 포스트 조회 기능 (1건)
+    // [ v ] Swagger Test Passed
+    // [ v ] return 형식 맞추기
     @GetMapping("/{postsId}")
     public ResponseEntity<Response<PostResponse>> getById(@PathVariable Long postsId) {
         PostResponse postResponse = postService.getById(postsId);
         return ResponseEntity.ok().body(Response.success(postResponse));
     }
 
-//    // [ V ] 포스트 조회 기능 (List, 20개)
-//    @GetMapping("")
-//    public ResponseEntity<List<PostResponse>> getAll(Pageable pageable) {
-//        return ResponseEntity.ok().body(postService.list(pageable));
-//    }
-
-    // [  ] 포스트 수정 기능 대충 틀만 짬 - 구현중
-//    @PutMapping("{postsId}")
-//    public ResponseEntity<PostEditResponse> editPost(@PathVariable Long postsId, @RequestBody PostEditRequest dto) {
-//        Optional<Post> postOptional = postRepository.findById(postsId);
-//        if (postOptional.isPresent()) { // postOptional이 있으면 수정
-//            PostEditResponse response = postService.edit(postsId, dto); // 파라미터 세 개 필요한거 맞나?
-//        } else{
-//            return "error";
-//        }
-//    }
-
-    // [ v ] 포스트 삭제 기능 - 구현
-    // [   ] return 형식 맞추기
-    @DeleteMapping("/post/{id}")
-    void delete(@PathVariable Long id) {
-        postRepository.deleteById(id);
+    // 📌
+    // [ v ] 포스트 조회 기능 (List, 20개)
+    // [ v ] Swagger Test Passed
+    // [ v ] return 형식 맞추기
+    @GetMapping("")
+    public ResponseEntity<Response<PostListResponse>> getAll(Pageable pageable) {
+        PostListResponse postListResponse = postService.list(pageable);
+        return ResponseEntity.ok().body(Response.success(postListResponse));
     }
 
+    // 📌
+    // [ v ] 포스트 수정 기능 구현
+    // [ v ] Swagger Test Passed
+    // [ v ] return 형식 맞추기
+    @PutMapping("/{postId}")
+    public ResponseEntity<Response<PostEditResponse>> updatePost(@PathVariable Long postId, @RequestBody PostEditRequest dto) {
 
+        PostEditResponse postEditResponse = postService.edit(postId, dto);
+
+        return ResponseEntity.ok().body(Response.success(postEditResponse));
+    }
+
+    // 📌
+    // [ v ] 포스트 삭제 기능
+    // [ v ] Swagger Test Passed
+    // [ v ] return 형식 맞추기
+    @DeleteMapping("/post/{id}")
+    public ResponseEntity<Response<PostDeleteResponse>> deletePost(@PathVariable Long id) {
+        PostDeleteResponse postDeleteResponse = postService.deletePostById(id);
+        return ResponseEntity.ok().body(Response.success(postDeleteResponse));
+    }
 }
