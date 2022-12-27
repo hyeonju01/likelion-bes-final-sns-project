b3122d81d03a5b27529d742970ca75961b3051af
hyeonju01 <tangled6648@gmail.com>
2022-12-27 15:37:57 +0900

    Feat: PostListResponse.java
    - DB에 저장된 Post 리스트를 반환

--- src/main/java/com/likelion/mutsasns/domain/dto/PostEditResponse.java
+++ src/main/java/com/likelion/mutsasns/domain/dto/PostEditResponse.java
@@ -1,4 +1,11 @@
 package com.likelion.mutsasns.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class PostEditResponse {
+    private Long UpdatedPostId;
+    private String message;
 }
