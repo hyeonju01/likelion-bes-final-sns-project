7be0e9fad7f85147b7870b0d45c9220078877f9e
hyeonju01 <tangled6648@gmail.com>
2022-12-27 15:37:24 +0900

    Feat: PostDeleteResponse.java
    - 삭제된 포스트 id와 삭제 성공 메세지 반환

--- src/main/java/com/likelion/mutsasns/domain/dto/PostDeleteResponse.java
+++ src/main/java/com/likelion/mutsasns/domain/dto/PostDeleteResponse.java
@@ -1,4 +1,15 @@
 package com.likelion.mutsasns.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostDeleteResponse {
+    private Long deletedPostId;
+    private String message;
 }
