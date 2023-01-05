7cbccc250c7fc0c6d6f5ce0da76eb035561c138c
hyeonju01 <tangled6648@gmail.com>
2023-01-05 11:38:47 +0900

    Chore: Dependency 변경

--- src/main/java/com/likelion/mutsasns/configuration/SwaggerConfiguration.java
+++ src/main/java/com/likelion/mutsasns/configuration/SwaggerConfiguration.java
@@ -1,14 +1,9 @@
 package com.likelion.mutsasns.configuration;
 
-import lombok.Builder;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
