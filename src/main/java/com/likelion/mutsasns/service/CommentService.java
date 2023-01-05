ce0ab3862cb76d9c6bb9d237fded8b16dc20ce56
hyeonju01 <tangled6648@gmail.com>
2023-01-05 11:27:22 +0900

    Feat: CommentService 추가

---
+++ src/main/java/com/likelion/mutsasns/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.likelion.mutsasns.service;
+
+import com.likelion.mutsasns.domain.dto.CommentAddRequest;
+import com.likelion.mutsasns.domain.dto.CommentAddResponse;
+import com.likelion.mutsasns.domain.entity.Comment;
+import com.likelion.mutsasns.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    // 댓글 등록
+    public CommentAddResponse add(Long postId, CommentAddRequest dto) {
+        Comment comment = dto.toEntity();
+        Comment registeredComment = commentRepository.save(comment); //save메서드의 결과, comment 객체 반환
+        CommentAddResponse commentAddResponse= new CommentAddResponse(postId, registeredComment); //
+        return commentAddResponse;
+    }
+
+}
