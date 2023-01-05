51ce0258b1258798c960764a42a38cb3e5c4aa2c
hyeonju01 <tangled6648@gmail.com>
2023-01-05 14:01:27 +0900

    Chore: @Hidden -> @ApiIgnore

--- src/main/java/com/likelion/mutsasns/controller/HelloController.java
+++ src/main/java/com/likelion/mutsasns/controller/HelloController.java
@@ -5,8 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
-@Hidden
+@ApiIgnore
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
