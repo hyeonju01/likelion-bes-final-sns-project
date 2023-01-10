9cf6552727a253c41c584b8b797b5039dd6e3394
hyeonju01 <tangled6648@gmail.com>
2023-01-10 17:08:06 +0900

    Chore: Add @ApiOperation annotation

--- src/main/java/com/likelion/mutsasns/controller/UserController.java
+++ src/main/java/com/likelion/mutsasns/controller/UserController.java
@@ -3,6 +3,7 @@ package com.likelion.mutsasns.controller;
 import com.likelion.mutsasns.domain.entity.Response;
 import com.likelion.mutsasns.domain.dto.*;
 import com.likelion.mutsasns.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -19,12 +20,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
+    @ApiOperation(value = "회원가입", notes = "회원가입합니다.")
     public ResponseEntity<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest) {
         UserJoinResponse userJoinResponse = userService.join(userJoinRequest);
         return ResponseEntity.ok().body(userJoinResponse);
     }
 
     @PostMapping("/login")
+    @ApiOperation(value = "로그인", notes = "본인의 Id와 password를 입력하여 로그인합니다.")
     public ResponseEntity<Response<UserLoginResponse>> login(@RequestBody UserLoginRequest userLoginRequest) {
         String token = userService.login(userLoginRequest.getUserName(), userLoginRequest.getPassword());
         UserLoginResponse loginResponse = new UserLoginResponse(token);
